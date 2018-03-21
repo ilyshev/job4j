13375671f9ad9bfceeac4c1468fb20f6f8b7c500
ilyshev <dilyshev@mac.com>
2018-03-21 21:14:46 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,21 @@
 package ru.job4j.calculate;
+
+/**
+* Calculate.
+*
+* @author Dmitry Ilyshev (dilyshev@mac.com)
+* @version $Id$
+* @since 0.1
+*/
+
 public class Calculate{
+
+	/**
+	* Конструктор, вывод строки в консоль
+	* @param args - args
+	* @return string Hello world
+	*/
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
