58fe04ebebc78678c6a8e9af6c066bf4f60e20d0
ilyshev <dilyshev@mac.com>
2018-03-23 18:08:37 +0500

    3.3. Идеальный вес.

--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -1,4 +1,29 @@
 package ru.job4j.calculator;
 
+/**
+ * Программа расчета идеального веса.
+ *
+ * @author Dmitry Ilyshev (dilyshev@mac.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
 public class Fit {
+    /**
+     * Вычисляем идеальный вес мужчины.
+     * @param height рост.
+     * @return идеальный вес.
+     */
+    public double manWeight(double height) {
+        return (height - 100) * 1.15;
+    }
+
+    /**
+     * Вычисляем идеальный вес женщины.
+     * @param height рост.
+     * @return идеальный вес.
+     */
+    public double womanWeight(double height) {
+        return (height - 110) * 1.15;
+    }
 }
+
--- chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -1,5 +1,36 @@
-import static org.junit.Assert.*;
+package ru.job4j.calculator;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Fit test
+ *
+ * @author Dmitry Ilyshev (dilyshev@mac.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
 public class FitTest {
+    /**
+     * проверка веса мужчин
+     */
+    @Test
+    public void whenManHeight180ThenWeight69() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(92.0, 0.1));
+    }
 
+    /**
+     * проверка веса женщин
+     */
+    @Test
+    public void whenWomanHeight170ThenWeight69() {
+        Fit fit = new Fit();
+        double weight = fit.womanWeight(170);
+        assertThat(weight, closeTo(69.0, 0.1));
+    }
 }
