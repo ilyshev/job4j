6859888b858c9f92131b2b565810dfbfc220d354
ilyshev <dilyshev@mac.com>
2018-03-27 16:06:45 +0500

    5.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,27 +1,49 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
 /**
- * Построить пирамиду в псеводографике.
+ * Построить пирамиду в псеводографике. Рефакторинг кода
  *
  * @author Dmitry Ilyshev (dilyshev@mac.com).
  * @version $Id$.
  * @since 0.1.
  */
+
 public class Paint {
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
+        StringBuilder screen = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (predict.test(row, column)) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
     /**
      * Рисуем правую сторону пирамиды.
      * @param height высота треугольника
      * @return результат
      */
     public String rightTrg(int height) {
-        //буфер для результата.
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+        /*//буфер для результата.
         StringBuilder screen = new StringBuilder();
         //ширина равна высоте.
-        int weight = height;
+        int width = height;
         //внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
             //внутренний определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
+            for (int column = 0; column != width; column++) {
                 //если строка равна ячейки, то рисуем галку.
                 if (row >= column) {
                     screen.append("^");
@@ -34,6 +56,7 @@ public class Paint {
         }
         //результат
         return screen.toString();
+        */
     }
     /**
      * Рисуем левую сторону пирамиды.
@@ -41,16 +64,21 @@ public class Paint {
      * @return результат
      */
     public String leftTrg(int height) {
-        //буфер для результата.
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+        /*//буфер для результата.
         StringBuilder screen = new StringBuilder();
         //ширина равна высоте.
-        int weight = height;
+        int width = height;
         //внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
             //внутренний определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
+            for (int column = 0; column != width; column++) {
                 //если строка равна ячейки, то рисуем галку.
-                if (row >= weight - column - 1) {
+                if (row >= width - column - 1) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -60,7 +88,7 @@ public class Paint {
             screen.append(System.lineSeparator());
         }
         //результат.
-        return screen.toString();
+        return screen.toString();*/
     }
 
     /**
@@ -69,14 +97,19 @@ public class Paint {
      * @return результат.
      */
     public String piramid(int height) {
-        //буфер для резульатата.
+       return this.loopBy(
+               height,
+               2 * height - 1,
+               (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+       );
+        /* //буфер для резульатата.
         StringBuilder screen = new StringBuilder();
         //ширина в два раза больше высоты.
-        int weight = 2 * height - 1;
+        int width = 2 * height - 1;
         //внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
             //внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
+            for (int column = 0; column != width; column++) {
                 //если строка равна ячейке, то рисуем галку.
                 if (row >= height - column - 1 && row + height - 1 >= column) {
                     screen.append("^");
@@ -88,6 +121,6 @@ public class Paint {
             screen.append(System.lineSeparator());
         }
         //результат.
-        return screen.toString();
+        return screen.toString();*/
     }
 }
